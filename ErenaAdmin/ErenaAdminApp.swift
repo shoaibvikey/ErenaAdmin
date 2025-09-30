8b5af6e1739ee32ebfaae90307144b0fd41e4ee7
shoaibvikey <shoaibakhtarvikey@gmail.com>
2025-09-30 18:59:27 +0530

    Initial Commit

---
+++ ErenaAdmin/ErenaAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErenaAdminApp.swift
+//  ErenaAdmin
+//
+//  Created by Shoaib Akhtar on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ErenaAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ErenaAdminTests/ErenaAdminTests.swift
@@ -0,0 +1,17 @@
+//
+//  ErenaAdminTests.swift
+//  ErenaAdminTests
+//
+//  Created by Shoaib Akhtar on 30/09/25.
+//
+
+import Testing
+@testable import ErenaAdmin
+
+struct ErenaAdminTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
